c13944e98e0b699a5914176fce5f3d719eded6bc
Ricky Yohanes Wijaya <rickyyohanes98@gmail.com>
2022-05-12 00:21:16 +0700

    Make Routing & htaccess file

--- app/cores/App.php
+++ app/cores/App.php
@@ -4,6 +4,22 @@ class App{
 
     public function __construct()
     {
-        echo 'OK';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL(){
+        if(isset($_GET['url'])){
+            // Remove Latest Slash in URL
+            $url = rtrim($_GET['url'],'/');
+
+            // Clean Up URL from SpecialChars
+            $url = filter_var($url,FILTER_SANITIZE_URL);
+
+            // Explode URL based on slash delimiter , String to Array
+            $url = explode('/',$url);
+            
+            return $url;
+        }
     }
 }
